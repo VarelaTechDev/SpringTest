77d213460ba6caa51d8d10288b3abdc96c3eee1f
VarelaTechDev <varela.tech.dev@gmail.com>
2023-05-19 08:20:07 -0700

    test

--- src/main/java/com/example/demotest/DemoTestApplication.java
+++ src/main/java/com/example/demotest/DemoTestApplication.java
@@ -11,3 +11,69 @@ public class DemoTestApplication {
     }
 
 }
+
+package com.example.demotest;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTestApplication.class, args);
+    }
+
+}
+package com.example.demotest;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTestApplication.class, args);
+    }
+
+}
+package com.example.demotest;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTestApplication.class, args);
+    }
+
+}
+package com.example.demotest;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTestApplication.class, args);
+    }
+
+}
+package com.example.demotest;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTestApplication.class, args);
+    }
+
+}
